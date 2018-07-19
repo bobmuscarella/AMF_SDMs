25cd071be536d0d86a94d0a5daf863f2680ce75a
Bob Muscarella <bob.muscarella@gmail.com>
2018-07-19 11:49:09 +0200

    Update Batch_run_AMF_SDMs.R

--- Batch_run_AMF_SDMs.R
+++ Batch_run_AMF_SDMs.R
@@ -26,10 +26,10 @@ occ_recs <- as.data.frame(readxl::read_excel('DATA/Locations_2018_03_17.xlsx'), 
 ############################################
 
 ### The function below will help to do this easily ### 
-taxloop <- function(occ_recs=NULL, envall=env_all, envclim=env_climate, envres=env_resources,
-                    minN=20, rank="MAARJAM_ID", method='checkerboard2', ncores=12,
-                    background="target"){
-  
+taxloop <- function(occ_recs=NULL, eall=env_all, eclim=env_climate, eres=env_resources,
+                    minN=20, rank="MAARJAM_ID", method='checkerboard2', algorithm='maxent.jar',
+                    background="target", parallel=F, ncores=12){
+
   namelist <- sort(unique(occ_recs[,rank]))
   namelist <- namelist[!is.na(namelist)]
   
@@ -37,85 +37,93 @@ taxloop <- function(occ_recs=NULL, envall=env_all, envclim=env_climate, envres=e
   for(i in seq_along(namelist)){
     
     foctax <- namelist[i]
-    warning(paste('now working on:', foctax, ':', i, 'of', length(namelist)), immediate. = T)
+    print(paste('now working on:', foctax, ':', i, 'of', length(namelist)), immediate. = T)
     
     fococc <- occ_recs[occ_recs[,rank] %in% foctax,]
-    if(background == "target"){
-      bgocc <- occ_recs[!occ_recs[,rank] %in% foctax,]
-    }
     
-    if(nrow(fococc) >= minN){
-      
-      fococcrows <- !is.na(rowSums(extract(envall, fococc[,c('Longitude','Latitude')])))
-      occ <- fococc[fococcrows,c('Longitude','Latitude')]
+    fococcrows <- !is.na(rowSums(extract(eall, fococc[,c('Longitude','Latitude')])))
+    occ <- fococc[fococcrows, c('Longitude','Latitude')]
+    
+    if(nrow(occ) >= minN){
       
       if(background == "target"){
-        focbgrows <- !is.na(rowSums(extract(envall, bgocc[,c('Longitude','Latitude')])))
-        bg <- bgocc[focbgrows,c('Longitude','Latitude')]
-        mod_all <- ENMevaluate(occ, envall, bg.coords=bg, method=method, parallel=T, numCores=ncores)#, algorithm=algorithm)
-        mod_climate <- ENMevaluate(occ, envclim, bg.coords=bg, method=method, parallel=T, numCores=ncores)#, algorithm=algorithm)
-        mod_resources <- ENMevaluate(occ, envres, bg.coords=bg, method=method, parallel=T, numCores=ncores)#, algorithm=algorithm)
+        bg <- bgocc[!is.na(rowSums(extract(eall, bgocc[,c('Longitude','Latitude')]))),c('Longitude','Latitude')]
+      } else {
+        mask <- sum(!is.na(eall)) == nlayers(eall)
+        values(mask)[values(mask)==F] <- NA
+        bg <- randomPoints(mask, 10000)
       }
-      
-      if(background != "target"){
-        mod_all <- ENMevaluate(occ, envall, method=method, parallel=T, numCores=ncores)#, algorithm=algorithm)
-        mod_climate <- ENMevaluate(occ, envclim, method=method, parallel=T, numCores=ncores)#, algorithm=algorithm)
-        mod_resources <- ENMevaluate(occ, envres, method=method, parallel=T, numCores=ncores)#, algorithm=algorithm)
+
+        mod_all <- ENMevaluate(occ, eall, bg.coords=bg, 
+                               method=method, parallel=parallel, 
+                               numCores=ncores, algorithm=algorithm, RM=1, fc='L')
+        mod_climate <- ENMevaluate(occ, eclim, bg.coords=bg, 
+                                   method=method, parallel=parallel, 
+                                   numCores=ncores, algorithm=algorithm, RM=1, fc='L')
+        mod_resources <- ENMevaluate(occ, eres, bg.coords=bg, 
+                                     method=method, parallel=parallel, 
+                                     numCores=ncores, algorithm=algorithm, RM=1, fc='L')
       }
-      
+
       mods_list[[i]] <- list(all=mod_all, climate=mod_climate, resources=mod_resources)
       names(mods_list)[i] <- foctax
-    }
   }
+  mods_list <- Filter(length, mods_list)
   return(mods_list)
 }
 
 
-### Now use the function to make models of each taxon in each rank
-order_models <- taxloop(occ_recs=occ_recs, rank='Order')
-family_models <- taxloop(occ_recs=occ_recs, rank='Family')
-genus_models <- taxloop(occ_recs=occ_recs, rank='Genus')
+genus_models <- taxloop(occ_recs=fiocc, rank='Genus')
+family_models <- taxloop(occ_recs=fiocc, rank='Family')
+order_models <- taxloop(occ_recs=fiocc, rank='Order')
 
-saveRDS(order_models, file='RESULTS/_order_taxa_results_27052018.RDS')
-saveRDS(family_models, file='RESULTS/_family_taxa_results_27052018.RDS')
-saveRDS(genus_models, file='RESULTS/_genus_taxa_results_27052018.RDS')
+
 
 
 ### TAXA MODELS (SAVE ONE AT A TIME)
-namelist <- table(occ_recs$MAARJAM_ID)
-namelist <- names(namelist)[namelist>=20]
 
-# for(i in seq_along(namelist)){
-for(i in 1:length(namelist)){
+namelist <- sort(unique(occ_recs[,rank]))
+namelist <- namelist[!is.na(namelist)]
+
+for(i in seq_along(namelist)){
   
   foctax <- namelist[i]
-  warning(paste('now working on:', foctax, ':', i, 'of', length(namelist)), immediate. = T)
+  print(paste('now working on:', foctax, ':', i, 'of', length(namelist)), immediate. = T)
   
-  fococc <- occ_recs[occ_recs[,'MAARJAM_ID'] %in% foctax,]
-  bgocc <- occ_recs[!occ_recs[,'MAARJAM_ID'] %in% foctax,]
-
-    fococcrows <- !is.na(rowSums(extract(env_all, fococc[,c('Longitude','Latitude')])))
-    occ <- fococc[fococcrows,c('Longitude','Latitude')]
-    
-    ### The following line selects background as all 'non-focal' occurrences
-    ### (a problem here is that there will be few background points for some models [e.g., the Glomerales order model])
-    focbgrows <- !is.na(rowSums(extract(env_all, bgocc[,c('Longitude','Latitude')])))
-    bg <- bgocc[focbgrows,c('Longitude','Latitude')]
-    
-    method='checkerboard2'
-    ncores=12
-    mod_all <- ENMevaluate(occ, env_all, bg.coords=bg, method=method, parallel=T, numCores=ncores)#, algorithm=algorithm)
-    mod_climate <- ENMevaluate(occ, env_climate, bg.coords=bg, method=method, parallel=T, numCores=ncores)#, algorithm=algorithm)
-    mod_resources <- ENMevaluate(occ, env_resources, bg.coords=bg, method=method, parallel=T, numCores=ncores)#, algorithm=algorithm)
-    
-    mod <- list(all=mod_all, climate=mod_climate, resources=mod_resources)
-    names(mod) <- foctax
+  fococc <- occ_recs[occ_recs[,rank] %in% foctax,]
   
-    filename <- paste0('RESULTS/', foctax, '.RDS')
-    saveRDS(mod, file=filename)
-
+  fococcrows <- !is.na(rowSums(extract(eall, fococc[,c('Longitude','Latitude')])))
+  occ <- fococc[fococcrows, c('Longitude','Latitude')]
+  
+  if(nrow(occ) >= minN){
+    
+    if(background == "target"){
+      bg <- bgocc[!is.na(rowSums(extract(eall, bgocc[,c('Longitude','Latitude')]))),c('Longitude','Latitude')]
+    } else {
+      mask <- sum(!is.na(eall)) == nlayers(eall)
+      values(mask)[values(mask)==F] <- NA
+      bg <- randomPoints(mask, 10000)
     }
-
+    
+    mod_all <- ENMevaluate(occ, eall, bg.coords=bg, 
+                           method=method, parallel=parallel, 
+                           numCores=ncores, algorithm=algorithm)
+    mod_climate <- ENMevaluate(occ, eclim, bg.coords=bg, 
+                               method=method, parallel=parallel, 
+                               numCores=ncores, algorithm=algorithm)
+    mod_resources <- ENMevaluate(occ, eres, bg.coords=bg, 
+                                 method=method, parallel=parallel, 
+                                 numCores=ncores, algorithm=algorithm)
+  }
+  
+  mods <- list()
+  mods[[1]] <- list(all=mod_all, climate=mod_climate, resources=mod_resources)
+  names(mods) <- foctax
+  
+  filename <- paste0('RESULTS/', foctax, '.RDS')
+  saveRDS(mods, file=filename)
+  
+}
 
 
 
